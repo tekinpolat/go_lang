03282044e421c5bc404ab2ac38d13751a394bf9b
Tekin POLAT <tekin.polat@ogr.dpu.edu.tr>
2021-03-15 23:01:00 +0300

    pointer tutorial 2

---
+++ ders_13/test.js
@@ -0,0 +1,20 @@
+function testarr(arr){
+    arr[0] = 21
+}
+
+arr = [1,2,3,4]
+
+testarr(arr)
+console.log(arr)
+
+
+let name = "tekin";   //immutable
+console.log(name[0]);
+
+name[0] = 'M';
+
+console.log(name);
+
+
+//immutable 
+//mutable
---
+++ ders_13/test.py
@@ -0,0 +1,17 @@
+
+def arrtest(arr):
+    arr[0] = 21
+    
+    
+arr = [2,3,54]
+arrtest(arr)
+
+print(arr)
+
+#call by reference
+
+name = "tekin"
+name[0] = 'M'
+print(name)
+
+#mutable vs immutable
